bc4cb61f09fc972ef7720b702caf8c2409febfae
VurdiOS <123585026+VurdIOS@users.noreply.github.com>
2023-12-11 22:13:19 +0300

    Create CollectionView on MainView with productCategorties

--- EcoMarket/MainView.swift
+++ EcoMarket/MainView.swift
@@ -8,23 +8,69 @@
 import UIKit
 
 class MainView: UIViewController {
+    
+    private let collectionView = UICollectionView(
+        frame: .zero,
+        collectionViewLayout: UICollectionViewFlowLayout()
+    )
+    
+    private let collectionsImages = [
+        UIImageView(image: UIImage(named: "CategoryCardFruits")),
+        UIImageView(image: UIImage(named: "CategoryCardDryFruits")),
+        UIImageView(image: UIImage(named: "CategoryCardVegetables")),
+        UIImageView(image: UIImage(named: "CategoryCardGrass")),
+        UIImageView(image: UIImage(named: "CategoryCardDrinks")),
+        UIImageView(image: UIImage(named: "CategoryCardMilk"))
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .MainBackgroundColor
-        title = "Эко Маркет"
-        navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.font: UIFont(name: "TTNormsPro-Bold", size: 24)!]
+        collectionView.register(
+            TypeProductsCollectionViewCell.self,
+            forCellWithReuseIdentifier: TypeProductsCollectionViewCell.id
+        )
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        view.addSubview(collectionView)
+        setupNavigationController()
+    }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        collectionView.frame = view.bounds
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setupNavigationController() {
+        title = "Эко Маркет"
+        navigationController?.navigationBar.titleTextAttributes = [
+            NSAttributedString.Key.font: UIFont(name: "TTNormsPro-Bold", size: 24)!
+        ]
     }
-    */
-
+}
+extension MainView: UICollectionViewDelegate {
+    
+}
+extension MainView: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // TODO - do not hardcoding
+        collectionsImages.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: TypeProductsCollectionViewCell.id,
+            for: indexPath
+        )
+        cell.backgroundView = collectionsImages[indexPath.item]
+        return cell
+    }
+}
+extension MainView: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: (((view.frame.size.width/2.1))*0.9222) , height: (view.frame.size.width/2.1))
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 10, left: 16, bottom: 0, right: 16)
+    }
 }
---
+++ EcoMarket/TypeProductsCollectionViewCell.swift
@@ -0,0 +1,39 @@
+//
+//  TypeProductsCollectionViewCell.swift
+//  EcoMarket
+//
+//  Created by Камаль Атавалиев on 11.12.2023.
+//
+
+import UIKit
+
+class TypeProductsCollectionViewCell: UICollectionViewCell {
+    static let id = "TypeProductsCollectionViewCell"
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.addSubview(imageView)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        imageView.frame = contentView.bounds
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        imageView.image = nil
+    }
+}
